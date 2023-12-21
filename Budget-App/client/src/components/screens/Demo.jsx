14509d31e8a00cb09e34aa2d937253c39b045782
camelclubcnft <118068699+ankhare8@users.noreply.github.com>
2023-12-21 07:10:25 -0500

    Update Demo.jsx

--- Budget-App/client/src/components/screens/Demo.jsx
+++ Budget-App/client/src/components/screens/Demo.jsx
@@ -4,7 +4,10 @@ export default function Demo() {
         <div className="grid place-items-center py-24 px-6 sm:py-32 lg:px-8">
           <div className="text-center">
             <h1 className="mt-4 text-3xl font-bold tracking-tight text-white-200 sm:text-5xl">Coming Soon!</h1>
-            <p className="mt-6 text-base leading-7 text-gray-200">A demo walk through of the application will be available shortly.<br/> Please sign up to explore our application or <a href="https://www.beautiful.ai/player/-NV0MmTVC6KjtKOZ3Dkt/Budgeting-App" target="blank">View our Project Report</a></p>
+            <p className="mt-6 text-base leading-7 text-gray-200">A demo walk through of the application will be available shortly.<br/> Please sign up to explore our application or 
+              <a href="https://mysterious-cover-7da.notion.site/Coming-Soon-1810f0a236984b219048f06af4487ffb?pvs=4" target="blank">
+              View our Project Report</a>
+            </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
                 href="/"
